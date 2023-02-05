b6131087eb6c3459c052e71c4448108c74a500ee
cindy <smile60250@gmail.com>
2023-02-05 14:12:38 +0800

    new project

---
+++ Electronic-business-platform/src/test/java/com/example/electronicbusinessplatform/ElectronicBusinessPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electronicbusinessplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicBusinessPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
